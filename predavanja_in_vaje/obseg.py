75d690bd8c56de99dbf2730716de678429c8ba2f
Tjasa Zilavec <tjasa.zilavec@gmail.com>
2025-12-23 19:17:24 +0100

    8

---
+++ predavanja_in_vaje/obseg.py
@@ -0,0 +1,21 @@
+import math
+
+
+def pravokotnik(a, b):
+    o = 2 * (a + b)
+    return o
+
+
+def kvadrat(a):
+    o = 4 * a
+    return o
+
+
+def krog(r):
+    o = 2 * math.pi * r
+    return o
+
+
+def enakostranični_trikotnik(a):
+    o = 3 * a
+    return o
---
+++ predavanja_in_vaje/ploscina.py
@@ -0,0 +1,21 @@
+import math
+
+
+def pravokotnik(a, b):
+    p = a * b
+    return p
+
+
+def kvadrat(a):
+    p = a ** 2
+    return p
+
+
+def krog(r):
+    p = math.pi * (r ** 2)
+    return p
+
+
+def enakostranični_trikotnik(a):
+    p = (math.sqrt(3) / 4) * (a ** 2)
+    return p
